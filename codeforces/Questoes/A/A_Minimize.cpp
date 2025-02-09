16ea4c2b93a921ef3bde01ad8fef8356b5095880
arthur <arthurmcb321@gmail.com>
2025-02-09 18:28:24 -0300

    Minimize

---
+++ codeforces/Questoes/A/A_Minimize.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int qtd; cin >> qtd;
+
+    while(qtd--){
+        int a, b; cin >> a >> b;
+
+        cout << b-a << "\n";
+    }
+}
