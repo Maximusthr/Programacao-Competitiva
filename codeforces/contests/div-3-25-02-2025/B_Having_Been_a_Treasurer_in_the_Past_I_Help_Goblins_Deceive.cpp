76ca82c5378c92cdfef1a4c0b924289e0229bd9b
Arthur Maximus <arthurmcb321@gmail.com>
2025-02-26 00:03:53 -0300

    Having Been a Treasurer in the Past, I Help Goblins Deceive

--- codeforces/contests/div-3-25-02-2025/B_Having_Been_a_Treasurer_in_the_Past_I_Help_Goblins_Deceive.cpp
+++ codeforces/contests/div-3-25-02-2025/B_Having_Been_a_Treasurer_in_the_Past_I_Help_Goblins_Deceive.cpp
@@ -4,37 +4,33 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(NULL);
-
     int t; cin >> t;
+
     while(t--){
-        int x; cin >> x;
-        string word; cin >> word;
+        int qtd; cin >> qtd;
+        string x; cin >> x;
 
-        int traco = 0;
-        int underline = 0;
+        string traco, linha;
 
-        for (int i = 0; i < x; i++){
-            if (word[i] == '_') underline++;
-            else traco++;
+        for (int i = 0; i < qtd; i++){
+            if (x[i] == '-') traco += x[i];
+            else linha += x[i];
+        }
+
+        if ((int)traco.size() <= 1 || ((int)linha.size() == 0)) {
+            cout << 0;
         }
         
-        if (traco <= 1 || underline == 0) cout << 0;
-        else if (traco == 2){
-            cout << underline;
-        }
-        else if (traco > underline){
-            cout << (underline*traco) + (underline*traco)/2;
-        }
+        // else if (traco.size() % 2 == 0){
+        //     cout << 1ll*(traco.size()/2 * traco.size()/2 * linha.size());
+        // }
+        // else {
+        //     cout << 1ll*(traco.size()/2) * (traco.size()/2 + 1) * linha.size();
+        // }
 
-        else if (underline == traco){
-            cout << underline*traco + underline;
-        }
-
-        else if (underline > traco){
-            cout << (underline * traco);
-        }
+        else cout << 1ll*(traco.size()/2 * linha.size() * (traco.size() - traco.size()/2));
 
         cout << "\n";
-    }
 
+    }
 }
