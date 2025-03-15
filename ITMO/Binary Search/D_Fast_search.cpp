26373168d6b486e8e613515117dd0db4f950bc4d
Arthur Maximus <arthurmcb321@gmail.com>
2025-03-15 17:54:18 -0300

    D - Fast Search

--- ITMO/Binary Search/D_Fast_search.cpp
+++ ITMO/Binary Search/D_Fast_search.cpp
@@ -4,77 +4,48 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(NULL);
-    int n; cin >> n;
-    int arr[n];
+    int qtd; cin >> qtd;
 
-    map<int, int> values;
+    vector<int> arr(qtd);
+    for (auto &i : arr) cin >> i;
 
-    for (int i = 0; i < n; i++){
-        cin >> arr[i];
-        values[arr[i]]++;
-    }
+    sort(arr.begin(), arr.end());
 
-    sort(arr, arr+n);
+    int q; cin >> q;
 
-    int k; cin >> k;
-    while(k--){
+    while(q--){
         int x, y; cin >> x >> y;
 
+        // lowerbound
         int l = 0;
-        int r = n-1;
-
-        int index_x = -1;
-        int index_y = -1;
-
-        //lowerbound em x
+        int r = (int)arr.size()-1;
+        int pos_lower = arr.size();
         while(l <= r){
-            // int mid = r - (r-l)/2;
             int mid = l + (r-l)/2;
 
             if (arr[mid] >= x){
-                index_x = mid;
                 r = mid - 1;
+                pos_lower = mid;
             }
-            else {
-                //index_x = mid;
-                l = mid + 1;
-            }
+            else l = mid + 1;
         }
 
+        //upperbound
         l = 0;
-        r = n-1;
-        //upperbound em y;
-        while (l <= r){
+        r = (int)arr.size()-1;
+        int pos_bigger = -1;
+        while(l <= r){
             int mid = l + (r-l)/2;
 
             if (arr[mid] <= y){
-                index_y = mid;
-                l = mid + 1;
+                l = mid+1;
+                pos_bigger = mid;
             }
-            else {
-                //index_y = mid;
-                r = mid - 1;
-            }
+            else r = mid-1;
         }
-
-        bool ok = false;
-        if (index_x >= 0 && values[arr[index_x]] > 1){
-            index_x += values[arr[index_x]] - 1;
-            ok = true;
-        }
-        if (index_y < n && values[arr[index_y]] > 1){
-            index_y += values[arr[index_y]] - 1;
-            ok = true;
-        }
-
-        if (index_x == index_y && arr[index_x] == x) cout << 1 << " ";
-        else if (index_x >= index_y && arr[index_x] != x) cout << 0 << " ";
-        else if (index_x != 0 && ok) cout << index_y - index_x << " ";
-        else if (!ok) cout << index_y - index_x + 1 << " ";
-        else cout << index_y << " ";
-
-        ok = false;
+        
+        
+        if (pos_bigger < pos_lower) cout << 0 << " ";
+        else cout << (pos_bigger - pos_lower + 1) << " ";
     }
-
-    cout << "\n";
 }
