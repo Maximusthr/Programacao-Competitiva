0b3c5737490d89899a3a4145dd84022deadcd01e
arthur <arthurmcb321@gmail.com>
2025-04-15 13:06:15 -0300

    The Ultimate Square

---
+++ codeforces/Questoes/A/A_The_Ultimate_Square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+
+    int t; cin >> t;
+
+    while(t--){
+        int x; cin >> x;
+
+        cout << (x+2-1)/2 << "\n";
+    }
+}
