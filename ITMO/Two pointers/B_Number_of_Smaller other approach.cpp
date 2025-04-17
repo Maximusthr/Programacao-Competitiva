d6f3effab0c9446256d893c5862c4524a2d93e1a
Arthur Maximus <arthurmcb321@gmail.com>
2025-04-16 22:26:50 -0300

    Number of Smaller other approach

---
+++ ITMO/Two pointers/B_Number_of_Smaller other approach.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int n, m; cin >> n >> m;
+
+    vector<int> a(n);
+    vector<int> b(m);
+
+    for (auto &i : a) cin >> i;
+    for (auto &i : b) cin >> i;
+
+    int j = 0;
+    for (int i = 0; i < m; i++){
+        while (j < n && b[i] > a[j]) j++;
+        cout << j << " ";
+    }
+    cout << "\n";
+}
