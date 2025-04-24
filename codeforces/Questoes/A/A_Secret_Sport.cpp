983c3f49df650cedfa1ad3b23ba598ce8c5333d1
arthur <arthurmcb321@gmail.com>
2025-04-24 14:02:02 -0300

    Secret Sport

---
+++ codeforces/Questoes/A/A_Secret_Sport.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        string s; cin >> s;
+
+        cout << s[(int)s.size()-1] << "\n";
+    }
+}
