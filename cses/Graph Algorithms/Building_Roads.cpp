7cc909f9cd6eb528d45bf452d75e0807e733ee37
arthur <arthurmcb321@gmail.com>
2025-04-24 15:46:09 -0300

    Building Roads

---
+++ cses/Graph Algorithms/Building_Roads.cpp
@@ -0,0 +1,53 @@
+// WA
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 2e5+1;
+
+vector<vector<int>> g(MAX);
+vector<bool> vis(MAX);
+
+void dfs(int u){
+    if (!vis[u]) vis[u] = true;
+
+    for (auto w : g[u]){
+        if (!vis[w]) {
+            vis[w] = true;
+            dfs(w);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+
+    int n, m; cin >> n >> m;
+    
+    for (int i = 0; i < m; i++){
+        int x, y; cin >> x >> y;
+        x--;
+        y--;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+
+    vector<int> index;
+    int sum = 0;
+    for (int i = 0; i < n; i++){
+        if (!vis[i]){
+            dfs(i);
+            sum++;
+            index.push_back(i+1);
+        }
+    }
+
+    if (sum >= 2){
+        cout << sum << "\n";
+        for (int i = 0; i < (int)index.size(); i++){
+            cout << index[i] << " \n" [i==index.size()-1];
+        }
+    }
+    else cout << 0 << "\n";
+}
