9dac2b7dfce47e1bcca342f70715332c1c518f86
Arthur Maximus <arthurmcb321@gmail.com>
2025-04-27 11:24:06 -0300

    Four Hidden - Solved

--- atcoder/contest/Beginner-27-04-2025/B_Four_Hidden.cpp
+++ atcoder/contest/Beginner-27-04-2025/B_Four_Hidden.cpp
@@ -11,58 +11,22 @@ int main(){
     ios_base::sync_with_stdio(0); cin.tie(NULL);
 
     string a, b; cin >> a >> b;
-
-    bool ok = false;
-    string c;
-    int cont = 0;
-    for (int i = 0, j = 0; i < a.size(); i++){
-        if (a[i] == '?') {
-            ok = true;
-            cont++;
+    
+    bool ok;
+    for (int i = 0; i < a.size(); i++){
+        string c = a.substr(i, a.size());
+        ok = true;
+        for (int j = 0; j < b.size(); j++){
+            if (c[j] != '?' && c[j] != b[j]){
+                ok = false;
+                break;
+            }
         }
         if (ok){
-            c += a[i];
-        }
-        if (cont == 4) break;
+            cout << "Yes"<< "\n";
+            break;
+        } 
     }
-
-    for (int i = c.size()-1, j = b.size()-1; i >= 0; i--){
-        if (c[i] == '?') {
-            c[i] = b[j];
-            if (j == -1) c[i] = '0';
-            else j--;
-        }
-        else {
-            j--;
-            continue;
-        }
-    }
-
-    ok = false;
-    cont = 0;
-    for (int i = 0, j = 0; i < a.size(); i++){
-        if (a[i] == '?') {
-            ok = true;
-            cont++;
-        }
-        if (ok){
-            a[i] = c[j];
-            j++;
-        }
-        if (cont == 4) break;
-    }
-
-    bool ans = false;
-    for (int i = 0, j = 0; i < a.size(); i++){
-        if (a[i] == b[j]){
-            j++;
-        }
-        else {
-            j = 0;
-        }
-        if (j == b.size()) ans = true;
-    }
-
-    if (ans) cout << "Yes" << "\n";
-    else cout << "No" << "\n";
+    
+    if (!ok) cout << "No" << "\n";
 }
