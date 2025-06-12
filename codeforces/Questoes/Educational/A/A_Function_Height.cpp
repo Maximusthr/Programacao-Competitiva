3f9014397ee51deb37232dc3b43f7b4aae16d486
Arthur Maximus <arthurmcb321@gmail.com>
2025-06-12 00:45:29 -0300

    Function Height

---
+++ codeforces/Questoes/Educational/A/A_Function_Height.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    ll n, k; cin >> n >> k;
+
+    cout << (k+n-1)/n << "\n";
+}
