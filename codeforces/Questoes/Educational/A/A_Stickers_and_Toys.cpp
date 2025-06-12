3b135df84777c3e5d087d80d149a2ea9800e7824
Arthur Maximus <arthurmcb321@gmail.com>
2025-06-12 16:31:45 -0300

    Stickers and Toys

---
+++ codeforces/Questoes/Educational/A/A_Stickers_and_Toys.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int t; cin >> t;
+
+    while(t--){
+        int n, s, b; cin >> n >> s >> b;
+
+        cout << n - min(s, b) + 1 << "\n";
+    }
+}
