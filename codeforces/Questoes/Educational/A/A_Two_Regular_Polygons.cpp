7ebba01c23ade59201128d34a521cd21a315cabc
Arthur Maximus <arthurmcb321@gmail.com>
2025-06-24 22:22:24 -0300

    Two Regular Polygons

---
+++ codeforces/Questoes/Educational/A/A_Two_Regular_Polygons.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int t; cin >> t;
+
+    while(t--){
+        int x, y; cin >> x >> y;
+
+        if (x % y == 0) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+    }
+}
