7001218680ed4f41a9d338b167128c2ff30a321d
Arthur Maximus <arthurmcb321@gmail.com>
2025-07-05 22:40:10 -0300

    The discrepant sensor

---
+++ codeforces/Others/2025 - MFP - First Phase/C_The_discrepant_sensor.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int a, b, c; cin >> a >> b >> c;
+
+    if (a == b) cout << c << "\n";
+    else if (a == c) cout << b << "\n";
+    else cout << a << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
