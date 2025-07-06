6451a459503af102ffabb5843e174ef68d72f043
Arthur Maximus <arthurmcb321@gmail.com>
2025-07-05 22:31:04 -0300

    Mixer for Phrases

---
+++ codeforces/Others/2025 - MFP - First Phase/G_Mixer_for_Phrases.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    string s; cin >> s;
+
+    string a, b;
+    for (int i = 0; i < s.size(); i++){
+        if (i % 2 == 0) a += s[i];
+        else b += s[i];
+    }
+
+    cout << a << "\n" << b << "\n";
+}
