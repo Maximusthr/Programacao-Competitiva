9ec3ba7a3cf99ded606f6f3e73c14e19306bcb19
Arthur Maximus <arthurmcb321@gmail.com>
2025-08-13 23:38:47 -0300

    Book Shop

---
+++ usaco/Gold/2 - Dynamic Programming/2 - Knapsack DP/Book_Shop.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int n, x; cin >> n >> x;
+ 
+    vector<int> price(n);
+    vector<int> page(n);
+ 
+    for (auto &i : price) cin >> i;
+    for (auto &i : page) cin >> i;
+ 
+    vector<int> dp(x+1);
+ 
+    for (int i = 0; i < n; i++){
+        for (int j = x; j >= price[i]; j--){
+            dp[j] = max(dp[j], page[i] + dp[j - price[i]]);
+        }
+    }
+ 
+    cout << dp[x] << "\n";
+}
