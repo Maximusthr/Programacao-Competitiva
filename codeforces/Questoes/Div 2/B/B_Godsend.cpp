b5c0f2ba9adbbc43b44783b00130b0aae78edca5
Arthur Maximus <arthurmcb321@gmail.com>
2025-08-14 22:13:07 -0300

    Godsend

---
+++ codeforces/Questoes/Div 2/B/B_Godsend.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int n; cin >> n;
+    
+    bool odd = false;
+    for (int i = 0; i < n; i++){
+        int x; cin >> x;
+
+        if (x % 2) {
+            cout << "First" << "\n";
+            return 0;            
+        }
+    }
+
+    cout << "Second" << "\n";
+}
