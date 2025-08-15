e637398edeeae44e904a676fee5722201b216ed3
Arthur Maximus <arthurmcb321@gmail.com>
2025-08-14 22:12:52 -0300

    Digits

---
+++ atcoder/contest/Avulso/B_Digits.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int n, k; cin >> n >> k;
+
+    int ans = 0;
+    while(n > 0){
+        n /= k;
+        ans++;
+    }
+
+    cout << ans << "\n";
+}
