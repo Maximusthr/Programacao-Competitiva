ae4b2b1aa46f586b5d618ae0d8c35caf6a016f7b
Arthur Maximus <arthurmcb321@gmail.com>
2025-09-10 16:53:26 -0300

    Div 3: 1047 - 07/09/2025

---
+++ codeforces/contests/div-3-1047-07-09-2025/A_Collatz_Conjecture.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int k, m; cin >> k >> m;
+
+    for (int i = 0; i < k; i++){
+        m *= 2;
+    }
+
+    cout << m << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
