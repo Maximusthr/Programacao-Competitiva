c046577631e1ef12fbf91bd3e3f0968aaff331f7
Arthur Maximus <arthurmcb321@gmail.com>
2025-09-29 00:37:26 -0300

    Game with Integers

---
+++ codeforces/Questoes/Div 3/A/A_Game_with_Integers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int x; cin >> x;
+
+    if (x % 3 == 0){
+        cout << "Second" << "\n";
+        return;
+    }
+    cout << "First" << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
