ab5ceac9effb29af79788bc438a3e2c72403f58b
Arthur Maximus <arthurmcb321@gmail.com>
2025-10-07 12:11:17 -0300

    Counting Divisors

---
+++ cses/7 - Mathematics/Counting_Divisors.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    int ans = 0;
+    for (int i = 1; i * i <= n; i++){
+        if (n % i == 0){
+            ans +=  i * i == n ? 1 : 2;
+        }
+    }
+
+    cout << ans << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
