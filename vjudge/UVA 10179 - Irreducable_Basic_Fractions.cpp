76e74ab436630bd37b01d5849261a9696492f4be
Arthur Maximus <arthurmcb321@gmail.com>
2025-10-08 16:05:57 -0300

    Irreducable Basic Fractions

---
+++ vjudge/UVA 10179 - Irreducable_Basic_Fractions.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int n;
+    while(cin >> n && n){
+        int ans = n;
+
+        for (int i = 2; i * i <= n; i++){
+            if (n % i == 0){
+                while (n % i == 0){
+                    n /= i;
+                }
+                ans -= ans/i;
+            }
+        }
+
+        if (n > 1){
+            ans -= ans/n;
+        }
+
+        cout << ans << "\n";
+    }
+}
