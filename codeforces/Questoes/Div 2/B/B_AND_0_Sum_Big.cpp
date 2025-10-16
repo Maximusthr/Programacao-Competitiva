d08a39e435ad25c6d1d39567b77601e197fc3c05
Arthur Maximus <arthurmcb321@gmail.com>
2025-10-16 16:11:24 -0300

    AND 0 Sum Big

---
+++ codeforces/Questoes/Div 2/B/B_AND_0_Sum_Big.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+const int MOD = 1e9 + 7;
+
+void solve(){
+    int n, k; cin >> n >> k;
+
+    ll ans = 1;
+    for (int i = 0; i < k; i++){
+        ans = (ans * n) % MOD;
+    }
+    cout << ans << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
