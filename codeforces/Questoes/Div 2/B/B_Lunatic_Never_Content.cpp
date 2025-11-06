9f7eb0a63c310dd7248525f13c14bfbc3b9a8435
Arthur Maximus <arthurmcb321@gmail.com>
2025-11-06 20:55:44 -0300

    Lunatic Never Content

---
+++ codeforces/Questoes/Div 2/B/B_Lunatic_Never_Content.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    vector<int> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    int valor = 0;
+    for (int i = 0; i < n; i++){
+        valor = __gcd(valor, abs(arr[i] - arr[n-i-1]));
+    }
+
+    cout << valor << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
