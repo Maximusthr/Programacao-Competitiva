4fc90f58f62f9ec80c74f9b59d20aecd626ef32b
Arthur Maximus <arthurmcb321@gmail.com>
2025-12-02 21:15:34 -0300

    Cat Cycle

---
+++ codeforces/Questoes/Educational/B/B_Cat_Cycle.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n, k; cin >> n >> k;
+    k--;
+
+    if (n % 2 == 0) cout << k % n + 1 << "\n";
+    else cout << (k + k/(n/2)) % n + 1 << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
