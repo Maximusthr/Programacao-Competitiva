e97f0e7396c4fa188cb9a1dcc04fe6c87c0bbbb1
Arthur Maximus <arthurmcb321@gmail.com>
2025-12-03 22:35:11 -0300

    Karina and Array

---
+++ codeforces/Questoes/Div 3/B/B_Karina_and_Array.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    vector<ll> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    sort(arr.begin(), arr.end());
+
+    ll ans = max(arr[0]*arr[1], arr[n-1]*arr[n-2]);
+
+    cout << ans << "\n";
+
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
