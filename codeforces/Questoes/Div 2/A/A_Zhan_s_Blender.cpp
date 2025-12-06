6b45ae0f272524b84d78f28727d5cc7b119fd8ca
Arthur Maximus <arthurmcb321@gmail.com>
2025-12-05 23:24:49 -0300

    Zhan's Blender

---
+++ codeforces/Questoes/Div 2/A/A_Zhan_s_Blender.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+    int x, y; cin >> x >> y;
+
+    cout << (n + min(x, y) - 1)/min(x, y) << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
