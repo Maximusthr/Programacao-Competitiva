6ae5ce7f41acc6b9189c36003e1a6c922b677747
Arthur Maximus <arthurmcb321@gmail.com>
2025-12-09 22:39:33 -0300

    Kevin and Combination Lock

---
+++ codeforces/Questoes/Div 1+2/A/A_Kevin_and_Combination_Lock.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int x; cin >> x;
+
+    cout << (x % 33 == 0 ? "YES" : "NO") << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
