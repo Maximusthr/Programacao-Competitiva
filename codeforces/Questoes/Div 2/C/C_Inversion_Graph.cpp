bb91c86d5e66583577b1989dca385e120ee05b13
Arthur Maximus <arthurmcb321@gmail.com>
2025-12-30 14:58:04 -0300

    Inversion Graph

---
+++ codeforces/Questoes/Div 2/C/C_Inversion_Graph.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    int ans = 0;
+    int x = 0;
+    for (int i = 1; i <= n; i++){
+        int y; cin >> y;
+        x = max(x, y);
+        if (x == i) ans++;
+    }
+    cout << ans << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
