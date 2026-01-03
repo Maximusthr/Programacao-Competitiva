092f06badc3a4ffe358b46a09989c52c45dc8ac1
Arthur Maximus <arthurmcb321@gmail.com>
2026-01-03 16:51:10 -0300

    Sum in Binary Tree

---
+++ codeforces/Questoes/Div 3/C/C_Sum_in_Binary_Tree.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    ll n; cin >> n;
+
+    ll sum = 0;
+    while (n > 0){
+        sum += n;
+        n >>= 1;
+    }
+
+    cout << sum << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
