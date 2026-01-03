bc30119a0d8d42289529c02f4135abe15f943929
Arthur Maximus <arthurmcb321@gmail.com>
2026-01-03 17:11:39 -0300

    Password

---
+++ codeforces/Questoes/Educational/A/A_Password.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    vector<int> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    int qtd = 10 - n;
+
+    int sum = 0;
+    for (int i = 0; i < qtd; i++){
+        for (int j = i+1; j < qtd; j++){
+            sum += 6;
+        }
+    }
+
+    cout << sum << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
