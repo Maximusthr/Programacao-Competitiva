7bc2c26b61696437ca38f4c88ce9b1c7b22450d7
Arthur Maximus <arthurmcb321@gmail.com>
2026-01-03 22:12:07 -0300

    Binary Cafe

---
+++ codeforces/Questoes/Div 3/B/B_Binary_Cafe.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n, k; cin >> n >> k;
+
+    k = min(k, 30);
+
+    cout << min(n+1, (1 << k)) << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
