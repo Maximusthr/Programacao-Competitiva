dbda456dd490f4ca47a86c375e898ff8c2ed3f92
Arthur Maximus <arthurmcb321@gmail.com>
2026-01-04 23:29:41 -0300

    Sum

---
+++ codeforces/Questoes/Div 4/A/A_Sum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int a, b, c; cin >> a >> b >> c;
+
+    if (a + b == c || a + c == b || b + c == a){
+        cout << "YES" << "\n";
+    }
+    else cout << "NO" << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
