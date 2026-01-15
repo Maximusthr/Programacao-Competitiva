80eb6ac87ec98500860dbaeb16e14000878ef9e7
arthur <arthurmcb321@gmail.com>
2026-01-15 20:05:18 -0300

    LCM Problem

---
+++ codeforces/Questoes/Educational/A/A_LCM_Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int l, r; cin >> l >> r;
+
+    if (l*2 <= r){
+        cout << l << " " << l*2 << "\n";
+    }
+    else cout << -1 << " " << -1 << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
