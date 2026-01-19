29fbb28ca90893fd5be9f6736885ac99af8b694c
arthur <arthurmcb321@gmail.com>
2026-01-19 13:02:23 -0300

    Bogosort

---
+++ codeforces/Questoes/Educational/B/B_Bogosort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    vector<int> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    sort(arr.begin(), arr.end(), greater<int> ());
+
+    for (auto &i : arr) cout << i << " ";
+    cout << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
