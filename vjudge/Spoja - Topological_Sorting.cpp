e57c04d7bca32a19f33b5d18cf86fd74fe3d6600
Maximusthr <arthurmcb321@gmail.com>
2026-04-03 00:27:23 -0300

    Topological Sorting

---
+++ vjudge/Spoja - Topological_Sorting.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n, m; cin >> n >> m;
+
+    vector<vector<int>> g(n);
+    vector<int> deg(n);
+    for (int i = 0; i < m; i++){
+        int x, y; cin >> x >> y;
+        x--, y--;
+        g[x].push_back(y);
+        deg[y]++;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < n; i++){
+        if (deg[i] == 0) pq.push(i);
+    }
+
+    vector<int> topo;
+    while(!pq.empty()){
+        int u = pq.top();
+        pq.pop();
+
+        topo.push_back(u);
+
+        for (auto v : g[u]){
+            deg[v]--;
+            if (deg[v] == 0) pq.push(v);
+        }
+    }
+
+    if (topo.size() != n){
+        cout << "Sandro fails." << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << topo[i] + 1 << " \n"[i==n-1];
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    solve();
+}
