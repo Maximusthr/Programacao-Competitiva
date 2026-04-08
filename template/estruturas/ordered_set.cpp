5bcec9691d2e8cb978a936c35d7a34be9e60c640
Maximusthr <arthurmcb321@gmail.com>
2026-04-07 21:08:36 -0300

    Ordered Set

---
+++ template/estruturas/ordered_set.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+template<class T> using ordered_set = 
+tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// less or less_equal (multi valores)
+// order_of_key()
+// find_by_order()
+
+void solve(){
+
+}   
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    solve();
+}
