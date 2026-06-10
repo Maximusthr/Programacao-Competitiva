32a3777fa6cda68228c8d26726a7dc2b4a6948f9
Maximusthr <arthurmcb321@gmail.com>
2026-06-10 17:59:04 -0300

    Armor

---
+++ atcoder/Avulso/ABC/A/A_Armor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n, m; cin >> n >> m;
+
+    if (n <= m) cout << "Yes" << "\n";
+    else cout << "No" << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    solve();
+}
