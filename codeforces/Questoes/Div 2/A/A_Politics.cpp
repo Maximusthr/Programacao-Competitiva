a5e1b25de6e813a4f058b6219f3aebe668aeaa97
Maximusthr <arthurmcb321@gmail.com>
2026-06-15 18:33:43 -0300

    Politics

---
+++ codeforces/Questoes/Div 2/A/A_Politics.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n, k; cin >> n >> k;
+
+    vector<string> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    int ans = 1;
+    for (int i = 1; i < n; i++){
+        if (arr[i] == arr[0]) ans++;
+    }
+
+    cout << ans << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
