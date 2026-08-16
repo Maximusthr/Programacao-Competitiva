38bc27a093f9c40e13994fc4b19a60380ae1ebb0
Maximusthr <arthurmcb321@gmail.com>
2026-08-16 12:55:17 -0300

    Kites

---
+++ codeforces/Questoes/Others/B_Kites.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(){
+    int n; cin >> n;
+
+    vector<int> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    sort(arr.begin(), arr.end());
+
+    vector<int> dif;
+    for (int i = 1; i < n; i++){
+        dif.push_back(arr[i] - arr[i-1]);
+    }
+
+    int ans = INF;
+    int prev = dif[0];
+
+    for (int i = 2; i < n-1; i++){
+        ans = min(ans, dif[i] + prev);
+        prev = min(prev, dif[i-1]);
+    }
+    
+    cout << ans << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
